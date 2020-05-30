72d4112c100ebd09965dfc729733172429f9d224
N1Anoobis <mrbond84@hotmail.com>
2020-05-30 16:19:17 +0200

    Add router and tags filtering

---
+++ src/components/features/TripListOptions/TripListOptionsContainer.js
@@ -0,0 +1,20 @@
+import {connect} from 'react-redux';
+import TripListOptions from './TripListOptions';
+import {getAllTags} from '../../../redux/tagsRedux';
+import {getAllFilters, changeSearchPhrase, changeDurationData, addCheckedTag, removeCheckedTag } from '../../../redux/filtersRedux';
+
+const mapStateToProps = state => ({
+  tags: getAllTags(state),
+  filters: getAllFilters(state),
+});
+
+const mapDispatchToProps = dispatch => ({
+  changeSearchPhrase: phrase => dispatch(changeSearchPhrase(phrase)),
+  // TODO - add more dispatchers for other filters
+  changeDurationData: data => dispatch(changeDurationData(data)),
+
+  addingTag: tag => dispatch(addCheckedTag(tag)),
+  removingTag: tag => dispatch(removeCheckedTag(tag)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(TripListOptions);
---
+++ src/redux/filtersRedux.js
@@ -0,0 +1,57 @@
+/* SELECTORS */
+
+export const getAllFilters = ({filters}) => filters;
+
+/* ACTIONS */
+
+// action name creator
+const reducerName = 'filters';
+const createActionName = name => `app/${reducerName}/${name}`;
+
+// action types
+export const CHANGE_PHRASE = createActionName('CHANGE_PHRASE');
+// TODO - add other action types
+export const CHANGE_DATA = createActionName('CHANGE_DATA');
+
+export const ADD_TAG = createActionName('ADD_TAG');
+export const REMOVE_TAG = createActionName('REMOVE_TAG');
+
+
+// action creators
+export const changeSearchPhrase = payload => ({ payload, type: CHANGE_PHRASE });
+// TODO - add other action creators
+export const changeDurationData = payload => ({payload, type: CHANGE_DATA});
+
+export const addCheckedTag = payload => ({payload, type: ADD_TAG});
+
+export const removeCheckedTag = payload => ({payload, type: REMOVE_TAG});
+// reducer
+export default function reducer(statePart = [], action = {}) {
+  switch (action.type) {
+    case CHANGE_PHRASE:
+      return {
+        ...statePart,
+        searchPhrase: action.payload,
+      };
+    case ADD_TAG:
+      console.log(statePart.tags);
+      return {
+        ...statePart,
+        tags: [
+          ...statePart.tags,
+          action.payload,
+        ],
+      };
+    case REMOVE_TAG:
+      console.log(statePart.tags);
+      return {
+        ...statePart,
+        tags:[
+          ...statePart.tags.filter(tag => tag!= action.payload),
+        ],
+      };
+    // TODO - handle other action types
+    default:
+      return statePart;
+  }
+}
