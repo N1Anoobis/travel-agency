d2c288ffe65103aebb706545c985e37990a2ca6f
N1Anoobis <mrbond84@hotmail.com>
2020-07-03 11:30:05 +0200

    Change let to const where needed

--- src/utils/formatTime.js
+++ src/utils/formatTime.js
@@ -1,7 +1,7 @@
 export const formatTime = (param) => {
   if(!param || isNaN(param) || param < 0) return null;
 
-  let sec = String(Math.floor(param%60));
+  const sec = String(Math.floor(param%60));
   const min = String(Math.floor((param/60)%60));
   const hrs = String(Math.floor(param/3600));
   return `${hrs.padStart(2, '0')}:${min.padStart(2, '0')}:${sec.padStart(2, '0')}`;
