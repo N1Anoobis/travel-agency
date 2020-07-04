9d4a8d31e0de6a7596b6ad685308888190755d46
N1Anoobis <mrbond84@hotmail.com>
2020-07-04 18:36:10 +0200

    Add more  optimization of component daysToSummer

--- src/components/features/daysToSummer/DaysToSummer.js
+++ src/components/features/daysToSummer/DaysToSummer.js
@@ -8,15 +8,18 @@ class DaysToSummer extends Component {
     super(props);
   }
 
-  getCountdownTime(){
-    let eventdate = moment(`${moment().year()}-06-22`);
+  getCountdownTime() {
+    const currentYear = moment().year();
+    let eventdate = moment(`${currentYear}-06-22`);
     const todaysdate = moment();
-    
-    if(todaysdate > (eventdate)){
-      eventdate = moment(`${moment().year()+1}-06-22`);
+
+    if (todaysdate > (eventdate)) {
+      eventdate = moment(`${currentYear + 1}-06-22`);
     }
 
-    if(todaysdate <= moment(`${moment().year()}-09-24`) && todaysdate >= moment(`${moment().year()}-06-21`)){
+    if (todaysdate <= moment(`${currentYear}-09-24`)
+      &&
+      todaysdate >= moment(`${currentYear}-06-21`)) {
       return null;
     } else {
       return eventdate.diff(todaysdate, 'days');
@@ -26,11 +29,16 @@ class DaysToSummer extends Component {
   render() {
     const daysNumber = this.getCountdownTime();
     return (
-      <div>
-        <h3 className={styles.countDown}>{daysNumber}{daysNumber ? <span className={styles.count}> {daysNumber == 1 ? 'Day to summer' : 'Days to summer'}</span> : null}</h3> 
-      </div>
+      <>
+        {daysNumber ?
+          <h3 className={styles.countDown}>{daysNumber}
+            <span className={styles.count}>
+              {daysNumber == 1 ? ' Day to summer' : ' Days to summer'}
+            </span>
+          </h3> : null}
+      </>
     );
-  } 
+  }
 }
 
 export default DaysToSummer;
--- src/components/features/daysToSummer/DaysToSummer.test.js
+++ src/components/features/daysToSummer/DaysToSummer.test.js
@@ -48,14 +48,47 @@ const checkDescriptionAtTime = (time, expectedDescription) => {
     expect(component.exists(select.countDown)).toEqual(true);
     global.Date = trueDate;
   });
+
+  it('should render without crashing', () => {
+    global.Date = mockDate(`${time}T00:00:00.135Z`);
+    const component = shallow(<DaysToSummer  />);
+    expect(component).toBeTruthy();
+    global.Date = trueDate;
+  });
 };
+
+const checkDescriptionAtSummer = (time, expectedDescription) => {
+  it(`should show correct at ${time}`, () => {
+    global.Date = mockDate(`${time}T00:00:00.135Z`);
   
+    const component = shallow(<DaysToSummer {...mockProps} />);
+    const renderedTime = component.find(select.countDown);
+    expect(renderedTime).toEqual(expectedDescription);
+  
+    global.Date = trueDate;
+  });
+  it('should render heading', () => {
+    global.Date = mockDate(`${time}T00:00:00.135Z`);
+    const component = shallow(<DaysToSummer />);
+    expect(component.exists(select.countDown)).toEqual(false);
+    global.Date = trueDate;
+  });
+
+  it('should render without crashing', () => {
+    global.Date = mockDate(`${time}T00:00:00.135Z`);
+    const component = shallow(<DaysToSummer  />);
+    expect(component).toBeTruthy();
+    global.Date = trueDate;
+  });
+};
+// all year but not summer 
 describe('Component HappyHourAd with mocked Date', () => {
   checkDescriptionAtTime('2020-06-20', '1 Day to summer');
-  checkDescriptionAtTime('2020-06-21', '');  
-  checkDescriptionAtTime('2020-09-22', '');
-  checkDescriptionAtTime('2020-09-23', ''); 
   checkDescriptionAtTime('2020-09-24', '270 Days to summer');
 });
-
-
+// during the summer
+describe('Component HappyHourAd with mocked Date', () => {
+  checkDescriptionAtSummer('2020-06-21', {});  
+  checkDescriptionAtSummer('2020-09-22', {});
+  checkDescriptionAtSummer('2020-09-23', {}); 
+});
